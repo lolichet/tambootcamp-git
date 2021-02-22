f990210ab2ba2c3d24f88cc7fb2cfd3075c8d1f5
lolichet <lolichet@lolichet-a01.vmware.com>
2021-02-22 13:58:36 +0100

    Update Comment #3

--- hello-world.go
+++ hello-world.go
@@ -1,6 +1,6 @@
 # First change my lolichet
 # add a second comment
-# add a third comment
+# Change made on my local branch
 # add a fourth comment
 # file:  hello-world.go
 # description:  my very first go program
